2507b2e0b1683c794b9a182c2249cf18beba7846
pein1625 <pein1625@gmail.com>
2019-12-04 16:41:45 +0700

    investvietnam

---
+++ investvietnam/js/globals.js
@@ -0,0 +1,82 @@
+// navbar mobile toggle
+$(function () {
+  var $body = $('html, body');
+  var $navbar = $('.js-navbar');
+  var $navbarOpen = $('.js-navbar-open');
+  var $navbarClose = $('.js-navbar-close');
+
+  $navbarOpen.on('click', function () {
+    $navbar.addClass('is-show');
+    $body.addClass('overflow-hidden');
+  });
+
+  $navbarClose.on('click', function () {
+    $navbar.removeClass('is-show');
+    $body.removeClass('overflow-hidden');
+  });
+});
+
+// menu toggle
+$(function () {
+  $(".menu-toggle").on("click", function () {
+    var $toggle = $(this);
+
+    $toggle.toggleClass("active").siblings(".menu-sub").slideToggle();
+
+    $toggle.siblings(".menu-mega").children(".menu-sub").slideToggle();
+
+    $toggle.parent().siblings(".menu-item-group").children(".menu-sub").slideUp();
+
+    $toggle.parent().siblings(".menu-item-group").children(".menu-mega").children(".menu-sub").slideUp();
+
+    $toggle.parent().siblings(".menu-item-group").children(".menu-toggle").removeClass("active");
+  });
+});
+
+$(function () {
+  addSwiper(".banner-slider", {
+    effect: "fade",
+    speed: 1000,
+    loop: true,
+    autoHeight: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false
+    },
+    pagination: {
+      type: "fraction"
+    }
+  });
+});
+
+// swiper template
+function addSwiper(selector, options = {}) {
+  return Array.from(document.querySelectorAll(selector), function (item) {
+    var $sliderContainer = $(item),
+        $sliderEl = $sliderContainer.find(selector + "__container");
+
+    if (options.navigation) {
+      $sliderContainer.addClass("has-nav");
+      options.navigation = {
+        prevEl: $sliderContainer.find(selector + "__prev"),
+        nextEl: $sliderContainer.find(selector + "__next")
+      };
+    }
+
+    if (options.pagination) {
+      $sliderContainer.addClass("has-pagination");
+      options.pagination = {
+        el: $sliderContainer.find(selector + "__pagination"),
+        clickable: true,
+        type: options.pagination.type || "bullets"
+      };
+    }
+
+    return new Swiper($sliderEl, options);
+  });
+}
+
+// common.js
+$(function () {
+  $('meta[name="viewport"]').prop("content", "width=1366");
+});
