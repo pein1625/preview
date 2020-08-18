eaa1732d8586c4a8d599a89ce5c1a3353b126d25
Pein 1625 <pein1625@gmail.com>
2020-08-18 17:10:14 +0700

    update hopestore

--- hopestore/js/app.js
+++ hopestore/js/app.js
@@ -83,6 +83,69 @@ $(function () {
   $(".js-post-content").find("table").addClass("table table-bordered table-hover mb-0").wrap('<div class="table-responsive"></div>');
 });
 
+// quantity input
+$(function () {
+  const $document = $(document);
+
+  $document.on("focus", ".quantity__input, [data-number-input]", function () {
+    const $input = $(this);
+    const min = $input.data("min") || 1;
+    const max = $input.data("max");
+    let currentValue = min;
+
+    if ($input.hasClass("is-binded")) return;
+
+    $input.addClass("is-binded");
+
+    $input.on("keydown", function (e) {
+      currentValue = $input.val();
+
+      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 || e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true) || e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true) || e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true) || e.keyCode >= 35 && e.keyCode <= 39) {
+        return;
+      }
+      if ((e.shiftKey || e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+      }
+    }).on("keyup", function (e) {
+      if ($input.val() == "") return;
+
+      let val = $input.val();
+
+      if (val === "" || parseInt(val) < min) {
+        $input.val(min);
+      }
+
+      if (parseInt(val) > max) {
+        $input.val(currentValue);
+      }
+
+      $input.trigger("change");
+    }).on("paste", function (e) {
+      var paste = e.originalEvent.clipboardData.getData("text");
+      var pasteNum = parseInt(paste);
+
+      if (pasteNum > max || pasteNum < min) {
+        e.preventDefault();
+      }
+    });
+  });
+
+  $document.on("click", ".quantity__btn", function (e) {
+    e.preventDefault();
+
+    var $siblingInput = $(this).siblings(".quantity__input");
+    var plus = $(this).data("plus");
+    var value = $siblingInput.val();
+    var newValue = parseInt(value) + plus;
+    var min = $siblingInput.data("min") || 1;
+
+    if (newValue >= min) {
+      $siblingInput.val(newValue);
+      $siblingInput.trigger("change");
+    }
+  });
+});
+
 // common.js
 document.addEventListener("DOMContentLoaded", function () {
   $(".js-receiver-info-checker").on("change", function () {
