a8fc875572d014482427cb53c0c5da27eb5c8411
Pein <pein1625@gmail.com>
2020-08-29 10:01:55 +0700

    add fujita page

---
+++ fujita/js/app.js
@@ -0,0 +1,131 @@
+// menu toggle
+$(function () {
+  $(".menu-toggle").on("click", function () {
+    var $toggle = $(this);
+
+    $toggle.toggleClass("active").siblings(".menu-sub").slideToggle();
+
+    $toggle.siblings(".menu-mega").children(".menu-sub").slideToggle();
+
+    $toggle.parent().siblings(".menu-item-group").children(".menu-sub").slideUp();
+
+    $toggle.parent().siblings(".menu-item-group").children(".menu-mega").children(".menu-sub").slideUp();
+
+    $toggle.parent().siblings(".menu-item-group").children(".menu-toggle").removeClass("active");
+  });
+});
+
+// navbar mobile toggle
+$(function () {
+  var $body = $("html, body");
+  var $navbar = $(".js-navbar");
+  var $navbarToggle = $(".js-navbar-toggle");
+
+  $navbarToggle.on("click", function () {
+    $navbarToggle.toggleClass("active");
+    $navbar.toggleClass("is-show");
+    $body.toggleClass("overflow-hidden");
+  });
+});
+
+// require _scroll-to-dip.js
+$(function () {
+  var $moveTop = $(".btn-movetop");
+  var $window = $(window);
+
+  if (!$moveTop.length) return;
+
+  $window.on("scroll", function () {
+    if ($window.scrollTop() > 150) {
+      $moveTop.fadeIn();
+
+      return;
+    }
+
+    $moveTop.fadeOut();
+  });
+});
+
+// swiper template
+function addSwiper(selector, options = {}) {
+  return Array.from(document.querySelectorAll(selector), function (item) {
+    var $sliderContainer = $(item),
+        $sliderEl = $sliderContainer.find(selector + "__container");
+
+    if (options.navigation) {
+      $sliderContainer.addClass("has-nav");
+      options.navigation = {
+        prevEl: $sliderContainer.find(selector + "__prev"),
+        nextEl: $sliderContainer.find(selector + "__next")
+      };
+    }
+
+    if (options.pagination) {
+      $sliderContainer.addClass("has-pagination");
+      options.pagination = {
+        el: $sliderContainer.find(selector + "__pagination"),
+        clickable: true
+      };
+    }
+
+    return new Swiper($sliderEl, options);
+  });
+}
+
+$(function () {
+  addSwiper(".banner-slider", {
+    pagination: true,
+    loop: true,
+    speed: 800,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    }
+  });
+});
+
+$(function () {
+  addSwiper(".news-slider", {
+    navigation: true,
+    direction: "vertical",
+    slidesPerView: 1,
+    freeMode: true,
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    spaceBetween: 10,
+    loop: true,
+    speed: 600,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    }
+  });
+});
+
+$(function () {
+
+  $(".js-post-content").find("table").addClass("table table-bordered table-hover mb-0").wrap('<div class="table-responsive"></div>');
+});
+
+// smooth scroll to div
+$(function () {
+  $(".js-scroll-to").on("click", function (e) {
+    e.preventDefault();
+
+    const $el = $(this),
+          target = $el.data("target") || $el.attr("href"),
+          offset = parseInt($el.data("offset")) || 0,
+          duration = parseInt($el.data("duration")) || 800;
+
+    if (!$(target).length) return;
+
+    $("html, body").animate({
+      scrollTop: $(target).offset().top - offset
+    }, duration);
+  });
+});
+
+// common.js
+$(function () {
+  new WOW().init();
+});
